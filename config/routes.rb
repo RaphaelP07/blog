6d64c12cfe8c81707589d206e7a8c470d84a909f
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-04-22 21:58:00 +0800

    added routes for article

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts
+
+  get '/articles' => 'articles#index'
+  get '/articles/new' => 'articles#new', as: 'new_article'
+  get '/articles/:id' => 'articles#show', as: 'show_article'
+  get '/articles/:id/edit' => 'articles#edit', as: 'edit_article'
+  post '/articles' => 'articles#create', as: 'create_article'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
